5dedfb8d9cdd682b3e398bfc54805316f705166e
alkuinvito <alkuinvito@tutanota.com>
2025-02-06 01:00:43 +0700

    build: add encryption helper

--- src/core/lib/crypto.ts
+++ src/core/lib/crypto.ts
@@ -1,12 +1,11 @@
-export function encrypt(data: string) {
-  return CryptoJS.AES.decrypt(data, process.env.HUB_SECRET as string).toString(
-    CryptoJS.enc.Utf8
+import * as crypto from "crypto-js";
+
+export function decrypt(data: string) {
+  return crypto.AES.decrypt(data, process.env.HUB_SECRET as string).toString(
+    crypto.enc.Utf8
   );
 }
 
-export function decrypt(data: string) {
-  return CryptoJS.AES.encrypt(
-    data,
-    process.env.HUB_SECRET as string
-  ).toString();
+export function encrypt(data: string) {
+  return crypto.AES.encrypt(data, process.env.HUB_SECRET as string).toString();
 }
